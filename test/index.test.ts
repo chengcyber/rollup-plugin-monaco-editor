7092f9345c7169482938a3d576ff960b47850c0f
Cheng Liu <liucheng.tech@outlook.com>
2021-07-27 14:51:50 +0800

    feat: support monaco-editor@0.26

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
 import { createTest } from './test-utils/test-creator';
 
-const monacoEditorVersions = ['0.21.2', '0.22.3', '0.24.0', '0.25.0'];
+const monacoEditorVersions = ['0.21.2', '0.22.3', '0.24.0', '0.25.0', '0.26.1'];
 
 for (const monacoEditorVersion of monacoEditorVersions) {
   createTest({
