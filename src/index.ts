9c3ac1ee3844eee7f4b9871f761e2b9e65b5f7b6
liucheng.leo <chengcyber@outlook.com>
2021-09-07 07:27:49 +0800

    chore: lint precommit

--- src/index.ts
+++ src/index.ts
@@ -481,13 +481,15 @@ function monaco(options: MonacoPluginOptions = {}): Plugin {
         );
 
         if (hasImportRegisterLanguage) {
-          const basicContribPath = resolveMonacoPath('vs/basic-languages/_.contribution');
+          const basicContribPath = resolveMonacoPath(
+            'vs/basic-languages/_.contribution'
+          );
           let basicContribCode = await fsp.readFile(basicContribPath, 'utf-8');
           // fillers/monaco-editor-core is same with editor.api, remove it
           basicContribCode = basicContribCode.replace(
-              /import\s+.*from ['"]\.\/fillers\/monaco-editor-core\.js['"];?/,
-              ''
-          )
+            /import\s+.*from ['"]\.\/fillers\/monaco-editor-core\.js['"];?/,
+            ''
+          );
           arr = arr.concat(basicContribCode);
         }
 
